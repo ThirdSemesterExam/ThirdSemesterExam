56a93c0b11694cbabdfec5d7d2a480172c90b7a0
Ferniss <mathiasf2000@hotmail.com>
2022-11-24 11:03:04 +0100

    Api program changes

--- API/Program.cs
+++ API/Program.cs
@@ -3,52 +3,57 @@ using Application.DTOs;
 using Application.Interfaces;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Console.WriteLine("initializing");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
 
+
+var mapper = new MapperConfiguration(configuration =>
+{
+    configuration.CreateMap<PostProductDTO, Pets>();
+}).CreateMapper();
+builder.Services.AddSingleton(mapper);
 
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlite(
     "Data source=db.db"
 ));
-//dependency resolver service. 
-//this AddScoped provides f-example this IProductService capable of being called through a dependency injection for other components inside of our system. 
-builder.Services.AddScoped<IPetsService, PetsService>();
-var mapper = new MapperConfiguration(configuration =>
-{
-    configuration.CreateMap<PostPetsDTO, Pets>();
-}).CreateMapper();
-builder.Services.AddSingleton(mapper);
 
+
+Application.DependencyResolver.DependencyResolverService.RegisterApplicationLayer(builder.Services);
+
+Infrastructure.DependencyResolver.DependencyResolverService.RegisterInfrastructure(builder.Services);
+
+builder.Services.AddCors();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-
+    app.UseCors(options => {
+        options.AllowAnyOrigin();
+        options.AllowAnyHeader();
+        options.AllowAnyMethod();
+    });
 }
 
-app.UseCors(options =>
-{
-    options.SetIsOriginAllowed(origin => true)
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials();
-});
+app.UseHttpsRedirection();
 
-app.UseAuthentication();     // enable UseAuthentication
-app.UseAuthorization();     // enable UseAuthorization
+app.UseAuthorization();
 
 app.MapControllers();
 
