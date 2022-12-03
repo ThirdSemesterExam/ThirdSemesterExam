cc54f524241b63cc666b1860feb341dad147189a
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-12-03 18:09:06 +0100

    create login components

--- Frontend/src/Services/http.service.spec.ts
+++ Frontend/src/Services/http.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+import jwtDecode from "jwt-decode";
 
-import { HttpService } from './http.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthguardService implements CanActivate {
 
-describe('HttpService', () => {
-  let service: HttpService;
+  constructor(private router: Router) { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
-  });
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let token = localStorage.getItem('token');
+    if(token) {
+      let decodedToken = jwtDecode(token) as Token;
+      let currentDate = new Date();
+      if(decodedToken.exp) {
+        let expiry = new Date(decodedToken.exp*1000);
+        if(currentDate<expiry && decodedToken.role=='Admin') {
+          return true;
+        }
+      }
+    }
+    this.router.navigate(['login'])
+    return false;
+  }
+}
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+
+class Token {
+  exp?: number;
+  role?: string;
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,15 +2,64 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Overlay} from "@angular/cdk/overlay";
+import { LoginComponent } from './login/login.component';
+import {RouterModule, RouterOutlet, Routes} from "@angular/router";
+//import {AuthguardService} from "../services/authguard.service";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+import {MyResolver} from "../services/http.service";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatMenuModule} from "@angular/material/menu";
+import {AuthguardService} from "../Services/http.service.spec";
+
+
+const routes: Routes = [
+  {
+    path: 'products', component: LoginComponent, canActivate: [AuthguardService], resolve: [MyResolver]
+  },
+  {
+    //path: 'login', component: LoginComponent
+  },
+
+  {
+    path: '**', redirectTo: 'test'
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [MatSnackBar, Overlay],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
