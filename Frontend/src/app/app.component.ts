edbf8cff54945963022ab73450f7db66f6f3691a
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-12-04 12:52:32 +0100

    register a user with password

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {customAxios, HttpService} from "../services/http.service";
+import {Router, Event, NavigationStart, NavigationEnd, NavigationError, ActivatedRoute} from '@angular/router';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import jwtDecode from "jwt-decode";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Frontend';
+  isMenuOpen = false;
+  toggleMenu():void{
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  currentRoute: string = "";
+
+  constructor(private router: Router,
+              public http: HttpService,
+              private snackBar: MatSnackBar) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+    let t = localStorage.getItem('token')
+    if (t) {
+      let decoded = jwtDecode(t) as any;
+      this.http.userName = decoded.email;
+    }
+  }
+
+  route() {
+    this.router.navigate(['./login'])
+   }
+
+
+  switchBetweenLoginAndProducts() {
+    if (this.currentRoute == '/login') {
+      this.router.navigate(['./products'])
+    } else if (this.currentRoute == '/products') {
+      this.router.navigate(['./login'])
+    }
+  }
+
+  logOut() {
+    this.router.navigate(['login']).then(() => {
+      this.snackBar.open('You have now been logged out', undefined, {duration: 3000})
+      localStorage.clear();
+      this.http.userName = undefined;
+    })
+  }
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,67 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Overlay} from "@angular/cdk/overlay";
+import { LoginComponent } from './login/login.component';
+import {FilterPipe, Pet} from './pets/pet';
+import {RouterModule, RouterOutlet, Routes} from "@angular/router";
+import {AuthguardService} from "../services/authguard.service";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+import {MyResolver} from "../services/http.service";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatMenuModule} from "@angular/material/menu";
+
+
+const routes: Routes = [
+  {
+    path: 'pets', component: Pet, canActivate: [AuthguardService], resolve: [MyResolver]
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+
+  {
+    path: '**', redirectTo: ''
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    Pet,
+    FilterPipe,
+
+   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [MatSnackBar, Overlay],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
