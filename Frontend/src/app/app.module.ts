e785d90ac29a8f96472b60b7c334ed60e3b0bf4d
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-12-07 14:20:38 +0100

    contact and about is works when you click  on it. its direct you the this chosen page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
     path: 'login', component: LoginComponent
   },
   {
-    path: 'about', component: AboutOsComponent
+    path: 'about-os', component: AboutOsComponent
   },
   {
     path: 'contact', component: ContactComponent
