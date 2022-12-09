9b57e91f91e02fa93c8f93841bf0b3643da73c00
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-12-09 12:52:38 +0100

    Update app.component.ts

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Router, Event, NavigationStart, NavigationEnd, NavigationError, Activate
 import {MatSnackBar} from "@angular/material/snack-bar";
 import jwtDecode from "jwt-decode";
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
